d831d89568338e73642476335c579364cc1823e0
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-08-03 19:04:57 +0100

    🚑 Fixed profile picture upload (#35)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { json, urlencoded } from 'express';
+import bodyParser from "body-parser";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -10,6 +12,9 @@ async function bootstrap() {
             whitelist: true, // Automatically remove properties that do not have any decorators
         }),
     );
+
+    app.use(json({ limit: '10mb' }));
+    app.use(urlencoded({ extended: true, limit: '10mb' }));
 
     // Enable CORS for all origins
     app.enableCors({
